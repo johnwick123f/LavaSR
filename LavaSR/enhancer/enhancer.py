ec6b65253b85e5be94fde52bbc9875ebacd74ee5
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2026-02-07 15:03:14 -0500

    Rename forward method to custom_forward
    Refactor forward method to custom_forward for clarity and maintainability. Updated argument and return documentation.

--- LavaSR/enhancer/enhancer.py
+++ LavaSR/enhancer/enhancer.py
@@ -3,36 +3,32 @@
 
 
 import torch
+import types
 from vocos import Vocos
 
 ## used to improve quality in end
 from LavaSR.enhancer.linkwitz_merge import FastLRMerge
 
 ## quick monkey patch to improve quality slightly
-def forward(x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass of the ISTFTHead module.
+def custom_forward(self, x: torch.Tensor) -> torch.Tensor:
+    """
+    Forward pass of the ISTFTHead module.
 
-        Args:
-            x (Tensor): Input tensor of shape (B, L, H), where B is the batch size,
-                        L is the sequence length, and H denotes the model dimension.
+    Args:
+        x (Tensor): Input tensor of shape (B, L, H)
 
-        Returns:
-            Tensor: Reconstructed time-domain audio signal of shape (B, T), where T is the length of the output signal.
-        """
-        self = vocos.head
-        x = self.out(x).transpose(1, 2)
-        mag, p = x.chunk(2, dim=1)
-        mag = torch.exp(mag)
-        mag = torch.clip(mag, max=1e3)  # safeguard to prevent excessively large magnitudes
-        # wrapping happens here. These two lines produce real and imaginary value
-        x = torch.cos(p)
-        y = torch.sin(p)
-
-    
-        S = mag * (x + 1j * y)
-        audio = self.istft(S)
-        return audio
+    Returns:
+        Tensor: Reconstructed time-domain audio signal
+    """
+    x = self.out(x).transpose(1, 2)
+    mag, p = x.chunk(2, dim=1)
+    mag = torch.exp(mag)
+    mag = torch.clip(mag, max=1e3)
+    x_real = torch.cos(p)
+    x_imag = torch.sin(p)
+    S = mag * (x_real + 1j * x_imag)
+    audio = self.istft(S)
+    return audio
   
 class LavaBWE:
     def __init__(self, model_path, device='cpu'):
@@ -40,6 +36,8 @@ class LavaBWE:
         self.device = device
         self.lr_refiner = FastLRMerge(device=device)
         self.bwe_model = Vocos.from_pretrained(model_path).eval().to(device)
+        self.bwe_model.head.forward = types.MethodType(custom_forward, self.bwe_model.head)
+
         
 
     def infer(self, wav, autocast=False):
